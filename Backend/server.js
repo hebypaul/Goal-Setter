9fb38e4a433125a3dd56d6c1e11b692afc426451
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-06 19:25:33 +0530

    get requset from post man

--- Backend/server.js
+++ Backend/server.js
@@ -4,5 +4,8 @@ const port = process.env.PORT || 5000
 
 
 const app = express()
+app.get('/api/goals',(req,res)=>{
+    res.json({"message":"Get goals"})
+})
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
