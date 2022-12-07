6a4796bdd870197c7de82c0026d4c8233e4c0f2a
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-07 06:45:28 +0530

    Error & Exception Handling

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -1,7 +1,39 @@
+// @desc Get goals
+// @route GET /api/goals
+// @access Private
+
 const getGoals = (req, res)=>{
     res.status(200).json({"messages": "Get goals"})
+}
+// @desc Set goal
+// @route POST /api/goals
+// @access Private
+
+const setGoal = (req, res)=>{
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(200).json({"message":"Set goals"})
+}
+// @desc Update goal
+// @route PUT /api/goals/:id
+// @access Private
+
+const updateGoal = (req, res)=>{
+    res.status(200).json({"message":`Update goal ${req.params.id}`})
+}
+// @desc Delete goal
+// @route DELETE /api/goals/:id
+// @access Private
+
+const deleteGoal = (req, res)=>{
+    res.status(200).json({"message":`Delete goal ${req.params.id}`})
 }
 
 module.exports = {
     getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
