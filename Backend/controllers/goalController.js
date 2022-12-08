16fbdfaafbdaa51306c3cd84c5c68c7a9c74f0a6
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-08 09:20:15 +0530

    Creat a Model

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+
+const Goal = require('../models/goalModel')
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
---
+++ Backend/models/goalModel.js
@@ -0,0 +1,12 @@
+const mongoose =require('mongoose')
+
+const goalSchema = mongoose.Schema({
+    text:{
+        type: String,
+        required:[true,'Please add atext value ']
+    }
+},{
+    timestamps:true,
+})
+
+module.exports = mongoose.model('Goal', goalSchema)
