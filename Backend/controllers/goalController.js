54d4ae3ce22aa5244371fec58f7d97460d1930c3
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-08 22:17:41 +0530

    Delete Goal

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -48,7 +48,15 @@ const updateGoal = asyncHandler( async (req, res) => {
 // @access Private
 
 const deleteGoal = asyncHandler( async (req, res)=>{
-    res.status(200).json({"message":`Delete goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+    
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+    
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
