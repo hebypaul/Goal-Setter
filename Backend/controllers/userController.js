140bb4caa49daf862441783c0efbdfa5e0ce5367
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-12 03:52:14 +0530

    Generate JWT

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -39,7 +39,8 @@ const registerUser =asyncHandler( async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -62,7 +63,8 @@ const loginUser = asyncHandler( async (req, res) => {
         res.json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -78,6 +80,15 @@ const loginUser = asyncHandler( async (req, res) => {
 const getMe = asyncHandler( async (req, res) => {
     res.json({message: 'User data'})
 })
+
+// Generate JWT
+
+const generateToken = (id) =>{
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+
+    })
+}
 
 module.exports = {
     registerUser,
