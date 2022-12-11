ecd3b033eee5c34f84cd2c0333e9a9663fe28cea
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-11 23:56:38 +0530

    Authenticate User

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -53,6 +53,21 @@ const registerUser =asyncHandler( async (req, res) => {
 //@access Public
 
 const loginUser = asyncHandler( async (req, res) => {
+    const {email, password } = req.body
+
+    //check for user email 
+    const user = await User.findOne({email})
+
+    if(user && ( await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
     res.json({message: 'Login User'})
 })
 
