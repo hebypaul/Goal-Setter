f92afff3b5a627e23dd6bef21701c88b509bc721
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-13 02:53:39 +0530

    delete  User Goal

--- Backend/controllers/goalController.js
+++ Backend/controllers/goalController.js
@@ -69,6 +69,20 @@ const deleteGoal = asyncHandler( async (req, res)=>{
         res.status(400)
         throw new Error('Goal not found')
     }
+    const user = await User.findById(req.user.id)
+
+    // Check for user
+    if(!user){
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Make sure the logged in user matches the goal user
+    if(goal.user.toString() !== user.id){
+        res.status(401)
+        throw new Error('User not authorized')
+    }
+    
     await goal.remove()
     
     res.status(200).json({id: req.params.id})
