ad526760c3dfe3615a2a5c52ab70a11da7bcb027
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-16 02:57:04 +0530

    Extra Reducers For Register

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -33,8 +33,22 @@ export const authSlice = createSlice({
             state.message = ''
         }
     },
-    extraReducers: () => {
-
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) =>{
+                state.isLoading = true
+            })
+            .addCase(register.fulfilled, (state, action) =>{
+                state.isLoading = false
+                state.isSuccess = true
+                state.user = action.payload
+            })
+            .addCase(register.rejected, (state , action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+                state.user = null
+            })
     }
 })
 
