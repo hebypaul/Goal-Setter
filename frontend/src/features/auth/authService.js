81f022d4271fcc291e06785c7d556783bac70ed0
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-16 09:54:43 +0530

    Login function

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,6 +13,17 @@ const register = async (userData) => {
 
     return response.data
 }
+// login user 
+
+const login = async (userData) => {
+    const response = await axios.post(API_URL + 'login', userData)
+
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
 
 // Logout user 
 const logout = () => {
@@ -20,6 +31,7 @@ const logout = () => {
 }
 const authService = {
     register , 
-    logout
+    logout,
+    login,
 }
 export default authService
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
+import {useSelector , useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom' 
+import {toast} from 'react-toastify'
+import {login, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
@@ -8,6 +14,26 @@ function Login() {
   })
 
   const {email, password} = formData
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+    )
+
+  useEffect(() => {
+    if(isError) {
+      toast.error(message)
+    }
+    if(isSuccess || user){
+      navigate('/')
+    }
+  
+    dispatch(reset())
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+
   const onChange = (e) => {
     setFormData((prevState)=> ({
       ...prevState,
@@ -17,6 +43,16 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault()
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData))
+  }
+
+  if(isLoading) {
+    return <Spinner />
   }
 
   return (
