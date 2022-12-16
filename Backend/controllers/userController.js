bfe2e755dd510018d0cf5c831e4683663e5f310c
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-16 18:29:45 +0530

    Backend fix

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -78,13 +78,7 @@ const loginUser = asyncHandler( async (req, res) => {
 //@access Private
 
 const getMe = asyncHandler( async (req, res) => {
-    const {_id, name, email}= await User.findById(req.user.id)
-
-    res.status(200).json({
-        id: _id,
-        name,
-        email
-    })
+    res.status(200).json(req.user)
 })
 
 // Generate JWT
