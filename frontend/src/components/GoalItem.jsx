e18fea1bfaff111b183944b012826a2db19732e5
Heby T Paul <hebytpaul1111@gmail.com>
2022-12-17 13:25:24 +0530

    Display Goals

---
+++ frontend/src/components/GoalItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function GoalItem({goal}) {
+  return (
+    <div className='goal'>
+        <div>
+            {new Date(goal.createdAt).toLocaleString('en-US')}
+        </div>
+        <h2>{goal.text}</h2>
+    </div>
+  )
+}
+
+export default GoalItem
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector, useDispatch} from 'react-redux'
 import GoalForm from '../components/GoalForm'
+import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
 import { getGoals, reset} from '../features/goals/goalSlice'
 
@@ -38,6 +39,15 @@ function Dashboard() {
         <p>Goals DashBoard</p>
       </section>
       <GoalForm/>
+      <section className="content">
+        {goals.length > 0 ? (
+          <div className="goals">
+            { goals.map((goal) => (
+              <GoalItem key={goal._id} goal={goal} />
+            ))}
+          </div>
+        ) : (<h3> You have not set any goals</h3>)}
+      </section>
     </>
   )
 }
